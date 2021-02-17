a3510d84d52b04ea33d8b3c4d20d92635f5955d8
r.zryachev <meg4eg@gmail.com>
2021-02-17 22:41:04 +0300

    fix lesson_08_2

--- templates/authoriz.php
+++ templates/authoriz.php
@@ -3,7 +3,7 @@
       <section class="content__side">
         <p class="content__side-info">Если у вас уже есть аккаунт, авторизуйтесь на сайте</p>
 
-        <a class="button button--transparent content__side-button" href="auth.php">Войти</a>
+        <a class="button button--transparent content__side-button" href="/auth.php">Войти</a>
       </section>
 
       <main class="content__main">
@@ -13,7 +13,7 @@
           <div class="form__row">
             <label class="form__label" for="email">E-mail <sup>*</sup></label>
             <?php $classname = isset($errors['email']) ? 'form__input--error' : ''; ?>
-            <input class="form__input <?= $classname; ?>" type="text" name="email" id="email" value="" placeholder="Введите e-mail">
+            <input class="form__input <?= $classname; ?>" type="text" name="email" id="email" value="<?=$_POST['email'] ?? ''; ?>" placeholder="Введите e-mail">
             <?php if (isset($errors['email'])): ?>
               <p class="form__message"><?= $errors['email']; ?></p>
             <?php endif; ?>
